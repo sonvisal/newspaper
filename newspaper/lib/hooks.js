cfdae745af4977de87af5e40b2fe63edea34965f
somalida <veasna.lida@gmail.com>
2016-01-20 04:46:52 +0700

    logo

--- newspaper/lib/hooks.js
+++ newspaper/lib/hooks.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var IR_BeforeHooks = {
     isAdmin: function(pause) {
       console.log('sreyden hook');   
@@ -9,57 +8,11 @@ var IR_BeforeHooks = {
           this.next();
         }
     },
-   /* isAdminOrMember:function(pause){
-=======
-// var IR_BeforeHooks = {
-//     isAdmin: function(pause) {
-//       console.log('djib hook');   
-//         if (!Roles.userIsInRole(Meteor.userId(), ['admin'],'mygroup')) {
-//           this.render('login');
-//           pause();
-//         }else{
-//           this.next();
-//         }
-//     },
-/*    isAdminOrMember:function(pause){
->>>>>>> ebfa7eb3651824f9b83834df33ab2929f4804bd1
-      if (!Roles.userIsInRole(Meteor.userId(), ['admin','member'],'mygroup')) {
-          this.render('login');
-          pause();
-      }else{
-          this.next();
-      }
-    }
 }
-<<<<<<< HEAD
 var routerNameAdmin=[
   'admin',
   'manageuser',
   'managepost',
   'profile'
 ];
-Router.before(IR_BeforeHooks.isAdmin, {only:routerNameAdmin});//for admin
-=======
-// var routerNameAdmin=[
-//   'admin',
-//   'manageuser',
-//   'profile'
-// ];
-/*var routerNameMember=[
-  'favorite',
-  'checkout',
-  'profile',
-  'editprofile',
-  'reward',
-  'member',
-  'advanced',
-  'dailyPopupf',
-  'confirmorder',
-  'confirmorder1',
-  'confirmorder2',
-  'payment'
-];*/
-// Router.before(IR_BeforeHooks.isAdmin, {only:routerNameAdmin});
-//for admin
->>>>>>> ebfa7eb3651824f9b83834df33ab2929f4804bd1
-//Router.before(IR_BeforeHooks.isAdminOrMember, {only:routerNameMember});//for member
+Router.before(IR_BeforeHooks.isAdmin, {only:routerNameAdmin});
