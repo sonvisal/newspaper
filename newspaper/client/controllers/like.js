3454d93afa649b7468af4f8d0b33fe0ff9257a9b
Phalla CHAB <phalla.chab@gmail.com>
2016-01-20 15:24:03 +0700

    user comment

--- newspaper/client/controllers/like.js
+++ newspaper/client/controllers/like.js
@@ -38,6 +38,7 @@ Template.home.helpers({
 });
 Session.set('like_login',"");
 Template.home.events({
+	
 	'click .glyphicon-thumbs-up':function(e){
         e.preventDefault();
         var content_id = this._id;
